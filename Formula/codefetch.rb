fd8124da9507b87dba201b7c229cf232bc991637
hsuktech <technical@handstand.co.uk>
2025-12-12 15:03:05 +0000

    Updates

--- Formula/codefetch.rb
+++ Formula/codefetch.rb
@@ -2,7 +2,7 @@ class Codefetch < Formula
   desc "CodeFetch CLI"
   homepage "https://codefetch.io"
   version "1.0.0"
-  url "https://dl.cloudsmith.io/public/hsops/codefetch/raw/names/codefetch-1.0.0.tar.gz"
+  url "https://dl.cloudsmith.io/public/hsops/codefetch/raw/file/codefetch.tar.gz"
   sha256 "7fd766e842170ac0aa7bfe56cd2f5e63a73af64a8a3c2ff777dc21dc6a157cc8"
   license "MIT"
 
