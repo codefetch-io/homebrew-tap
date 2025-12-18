70551dc900a22aec74179131bcae933b56733252
hsuktech <technical@handstand.co.uk>
2025-12-18 18:58:53 +0000

    Update codefetch to version 1.0.0

--- Formula/codefetch.rb
+++ Formula/codefetch.rb
@@ -3,7 +3,7 @@ class Codefetch < Formula
   homepage "https://codefetch.io"
   version "1.0.0"
   url "https://dl.cloudsmith.io/public/hsops/codefetch/raw/files/codefetch.tar.gz"
-  sha256 "e31cdde16eda3c5f7821ec0a3a27298c466bd11491f0a25066688a3228b0e9de"
+  sha256 "8b7cfb0fc54c13923de7c57f5370f7ea8d64f95fec52dcf261bee0da52bab76c"
   license "MIT"
 
   def install
